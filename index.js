931d70f23e24aa06b3e61b4ba3d006a773f3176d
Kawana T Small <jazzytech@Kawanas-MacBook-Pro.local>
2022-07-01 10:16:33 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution in this file!
+var customerName ='bob'
+console.log (customerName)
+function upperCaseCustomerName(){
+  console.log (customerName.toUpperCase())
+  customerName = customerName.toUpperCase()
+}
+ function setBestCustomer(){ 
+  console.log ('not bob')
+  bestCustomer = 'not bob'
+ }
+ function overwriteBestCustomer(){
+  console.log ('maybe bob')
+  bestCustomer = 'maybe bob'
+ }
+ const leastFavoriteCustomer = 'R Kelly'
+ function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'G Maxwell'
+ }
+
+ 
+ 
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
